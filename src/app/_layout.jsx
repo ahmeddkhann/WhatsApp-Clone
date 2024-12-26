4a9b40c16a4b6cb575232dbbf55359be77631e0c
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-12-26 15:36:59 +0500

    initially done with project

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,22 +1,20 @@
-import { Stack, Redirect } from "expo-router";
-import * as SplashScreen from "expo-splash-screen"
-import { useEffect, useState} from "react";
+import React, {useState, useEffect} from "react";
+import { Redirect, Stack } from "expo-router";
 
-SplashScreen.preventAutoHideAsync();
 
-export default function Layout() {
 
-  const [isLogin, setIsLogin] = useState(false)
-  useEffect (() => {
-      SplashScreen.hideAsync();
-  },[])
+const RootLayout = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
 
   return (
-  <>
-      <Stack  screenOptions={{headerShown: false}}/>
-      {
-        isLogin? <Redirect href={"/(main)"}/> : <Redirect href={"/(auth)"}/>
+    <>
+      <Stack screenOptions={{ headerShown: false}}/>
+      {isLogin? <Redirect href={"/(main)"}/>:  
+        <Redirect href={"/(auth)"}/>
       }
     </>
-  );
+  )
 }
+
+export default RootLayout
