2c25cfb933bcf9b35c7d8b4fa921ed30038c0b94
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-12-26 20:57:34 +0500

    section completed

--- src/app/(auth)/welcome.jsx
+++ src/app/(auth)/welcome.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import images from '@/src/constants/ImagePath'
 
@@ -6,14 +6,17 @@ const Welcome = () => {
   return (
    <SafeAreaView style={styles.container}>
     <View style={styles.header}>
-      <Text> Welcome to Whatsapp</Text>
+      <Text style={styles.headerText}> Welcome to WhatsApp</Text>
       <Image  
        source={images.termsAgree}
        style={styles.termsAgree}
       />
-      <Text> Read our <Text style={styles.betweenText}> Privacy Policy</Text> Tap "Agree and Continue" to accept the 
-       <Text style={styles.betweenText}>Terms of Services</Text>
+      <Text style={styles.paragraphText}> Read our <Text style={styles.betweenText}> Privacy Policy</Text> Tap "Agree and Continue" to accept the 
+       <Text style={styles.betweenText}> Terms of Services</Text>
       </Text>
+      <TouchableOpacity style={styles.agreeButton}>
+        <Text style={styles.agreeButtonText}> Agree and Continue</Text>
+      </TouchableOpacity>
     </View>
     <View style={styles.footer}></View>
    </SafeAreaView>
@@ -30,17 +33,43 @@ const styles = StyleSheet.create({
   },
   header: {
     height: "75%",
-    width: "100%"
+    width: "100%",
+    alignItems: "center",
   },
   footer: {
     height: "25%",
     width: "100%"
   },
   termsAgree: {
-    height: 100,
-    width: 100,
+    height: 220,
+    width: 220,
+    marginTop: 30
   },
   betweenText: {
     color: "blue"
+  },
+  agreeButton: {
+   backgroundColor: "#1E9B50",
+    borderRadius: 10,
+    padding: 10,
+    width: "80%",
+    marginTop: 20
+  },
+  agreeButtonText: {
+    color: "white",
+    fontSize: 15,
+    textAlign: "center"
+  },
+  headerText: {
+    fontSize: 24,
+    color: "black",
+    fontWeight: "bold",
+    marginTop: 54
+  },
+  paragraphText: {
+    fontSize: 15,
+    color: "black",
+    marginTop: 30,
+    width: "80%"
   }
 })
