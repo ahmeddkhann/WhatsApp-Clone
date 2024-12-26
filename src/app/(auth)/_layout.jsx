96a04db8e3481ea049be61122116a04d433de898
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-12-26 15:48:18 +0500

    one section completed

--- src/app/(auth)/_layout.jsx
+++ src/app/(auth)/_layout.jsx
@@ -5,7 +5,7 @@ export default function Layout() {
     return (
         <Stack>
             <Stack.Screen name="index"  
-             options={{title: "Auth Page"}}
+             options={{headerShown: false}}
             />
         </Stack>
     )
--- src/app/(auth)/index.jsx
+++ src/app/(auth)/index.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
+
 
 const HomePage = () => {
   return (
-    <View>
-      <Text>Welcome to Whatsap Auth Section!!</Text>
+  <SafeAreaView style={styles.container}>
+    <View ></View>
+    <View style={styles.body}></View>
+    <View style={styles.footer}>
+      <Text style={styles.fromText}>From</Text>
+      <Text style={styles.facebookText}>Facebook</Text>
     </View>
+  </SafeAreaView>
   )
 }
 
 export default HomePage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 25
+  },
+
+  body: {
+    height: 50,
+    width: 50,
+    backgroundColor: "blue"
+  },
+  footer: {
+   alignItems: "center"
+},
+  fromText: {
+    fontSize: 12
+  },
+  facebookText: {
+    fontSize: 15,
+    fontWeight: "bold"
+  }
+})
